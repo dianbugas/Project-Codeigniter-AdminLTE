e56e7e2c2407b92dc0e5bb6d6668fc10e1c6751a
dianynf <dianbugas@gmail.com>
2019-01-26 15:06:58 +0700

    pembaharuan

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,2 +1,17 @@
-<h1>Selamat datang</h1>
-<div class="container"></div>
+<div class="alert alert-block alert-success">
+								<button type="button" class="close" data-dismiss="alert">
+									<i class="ace-icon fa fa-times"></i>
+								</button>
+
+								<i class="ace-icon fa fa-check green"></i>
+                                <h1>Selamat datang</h1>
+								Welcome to yuguygu
+								<strong class="green">
+									Ace
+									<small>(v1.4)</small>
+								</strong>,
+								лёгкий, многофункциональный и простой в использовании шаблон для админки на bootstrap 3.3.6. Загрузить
+								исходники с <a href="https://github.com/bopoda/ace">github</a> (with minified ace js/css files).
+							</div>
+				</div><!-- /.page-content -->
+			</div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,9 +1,12 @@
+		</div><!-- /.page-content -->
+	</div>
+</div><!-- /.main-content -->
+		
 <div class="footer">
 			<div class="footer-inner">
 				<div class="footer-content">
 					<span class="bigger-120">
-						<span class="blue bolder">Ace</span>
-						Application &copy; 2013-2014
+						Muhammad Ardiansyah &copy; 2013-2014
 					</span>
 
 					&nbsp; &nbsp;
@@ -23,12 +26,10 @@
 				</div>
 			</div>
 		</div>
-
 		<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
 			<i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
 		</a>
-	</div><!-- /.main-container -->
-
+	</div><!-- /.main-container -->	
 <!-- basic scripts -->
 
 		<!--[if !IE]> -->
